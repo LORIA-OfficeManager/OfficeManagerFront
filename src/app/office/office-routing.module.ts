82baa5250868cd2e5a6e38f9d13e6a5221a05e73
formotiis <nosvulca@gmail.com>
2019-12-06 15:52:11 +0100

    Made test route public

--- src/app/office/office-routing.module.ts
+++ src/app/office/office-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {TestComponent} from './temporary/test/test.component';
 import {OfficeComponent} from './office.component';
 import {ListOfficeComponent} from './temporary/list-office/list-office.component';
-import {DevGuardService} from './shared/services/guards/dev-guard.service';
+// import {DevGuardService} from './shared/services/guards/dev-guard.service';
 import {UserGuardService} from './shared/services/guards/user-guard.service';
 
 const routes: Routes = [{
@@ -12,7 +12,7 @@ const routes: Routes = [{
   children: [
     {
       path: 'test',
-      canActivate: [DevGuardService], // Only during dev mode, not production
+      // canActivate: [DevGuardService], // Only during dev mode, not production
       component: TestComponent,
     },
     {

68f492454e9eac96110ca7ef28484abc5b35ff46
Highnewb <thouveninvalentin@gmail.com>
2019-12-07 10:10:56 +0100

    Fix script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /deploy/officemanager-front
+yarn install
 ng build --prod
 rm -rf /var/wwww/OfficeManager/officemanager-app/*
 cp -r dist/* /var/www/OfficeManager/officemanager-app/

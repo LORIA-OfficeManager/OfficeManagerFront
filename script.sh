8294a22c1a91f67d021cb820cb28b27ae51544d4
Highnewb <thouveninvalentin@gmail.com>
2019-12-07 10:47:06 +0100

    Refix travis & script

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd /deploy/officemanager-front
-yarn install
-ng build --prod
 rm -rf /var/wwww/OfficeManager/officemanager-app/*
 cp -r dist/* /var/www/OfficeManager/officemanager-app/
 systemctl restart apache2

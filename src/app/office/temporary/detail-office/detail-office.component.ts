4ba55aad52d90b3a61894488ebe5c7494daa4e2c
Gauthier Brin <gauthier.brin8@etu.univ-lorraine.fr>
2019-12-12 08:54:09 +0100

    j'ai oublié de add les nouveaux componenents

---
+++ src/app/office/temporary/detail-office/detail-office.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {NbWindowRef} from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-detail-office',
+  templateUrl: './detail-office.component.html',
+  styleUrls: ['./detail-office.component.scss'],
+})
+export class DetailOfficeComponent implements OnInit {
+
+  private _data: any;
+  constructor(public windowRef: NbWindowRef) {
+    this._data = windowRef.config.context;
+  }
+
+  close() {
+    this.windowRef.close();
+  }
+  ngOnInit() {
+  }
+  get data(): any {
+    return this._data;
+  }
+}

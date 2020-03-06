23981609bb0d764a0dbf1d1f14508c918d2557c7
formotiis <nosvulca@gmail.com>
2020-03-06 15:51:08 +0100

    auth module restore

---
+++ src/app/office/auth/auth-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {NbAuthComponent} from '@nebular/auth';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './logout/logout.component';
+import {UserGuardService} from '../shared/services/guards/user-guard.service';
+import {GuestGuardService} from '../shared/services/guards/guest-guard.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        canActivate: [GuestGuardService], // Only if user is a guest
+        component: LoginComponent,
+      },
+      {
+        path: 'login',
+        canActivate: [GuestGuardService], // Only if user is a guest
+        component: LoginComponent,
+      },
+      {
+        path: 'logout',
+        canActivate: [UserGuardService], // Only Users or above
+        component: LogoutComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {
+}
